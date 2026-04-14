82d5704c7ac235e0bdf50afa12a468a0d13bf5c5
Florea Alina <floreaalina340@gmail.com>
2026-04-14 12:23:53 +0300

    Init app

---
+++ src/Student.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Student {
+    public static void main(String[] args) {
+
+
+        System.out.println("Intrare in program...");
+
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
