77f8f0039fa654a48d0c3c3a1597c9a9525e8ad6
Florea Alina <floreaalina340@gmail.com>
2026-04-15 15:47:12 +0300

    Add student list

--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,6 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Student {
@@ -9,8 +10,9 @@ public class Student {
         System.out.println("Intrare in program...");
 
         Scanner scan = new Scanner(System.in);
+        ArrayList<String> studenti = new ArrayList<>();
 
-
+        //Facultate
         while (true) {
             System.out.print("Introducere facultate(Matematica/Informatica): ");
             String facultate = scan.nextLine();
@@ -28,76 +30,88 @@ public class Student {
         }
 
         System.out.print("Doriti sa introduceti studenti noi? ");
-
         String decizie = scan.nextLine();
 
-        if(decizie.equalsIgnoreCase("da")) {
+        if (decizie.equalsIgnoreCase("da")) {
             System.out.println("Creati numele grupei noi");
-        }else {
+        } else {
             System.out.println("Iesire din program...");
             return;
         }
 
+        while(true){
         System.out.print("Numele grupei: ");
-
         String letter = scan.nextLine();
         int number = scan.nextInt();
+        scan.nextLine();
 
         System.out.println(letter + number);
 
-        scan.nextLine();
 
-        System.out.print("Introduceti numele studentului: ");
-         String name = scan.nextLine();
+        String name;
+        while (true) {
+            System.out.print("Introduceti numele studentului: ");
+            name = scan.nextLine();
 
-         while(true) {
-
-             System.out.print("Introduceti anul nasterii studentului: ");
-             int yearOfBirth = scan.nextInt();
-
-             int curentYear = LocalDate.now().getYear();
-
-             int age = curentYear - yearOfBirth;
-
-             if (age <= 0 || age > 120)
-                 System.out.println("Reintroduceti anul nasterii: ");
-             else {
-                 System.out.println("Student: " + name + " " + "Varsta: " + age + " ani.");
-                 break;
-             }
+            if (!name.matches("[a-zA-Z]+"))
+                System.out.println("Nume invalid! Reintroduceti numele");
+            else
+                break;
+        }
 
 
-         }
+        int age;
+        while (true) {
 
-         System.out.print("Introduceti specialitatea(Algebra/ Geometrie/ Calcul_Integral: ");
-         scan.nextLine();
-         String specialitate = scan.nextLine();
+            System.out.print("Introduceti anul nasterii studentului: ");
+            int yearOfBirth = scan.nextInt();
 
-         if(specialitate.equalsIgnoreCase("algebra") || specialitate.equalsIgnoreCase("geometrie") || specialitate.equalsIgnoreCase("calcul_integral")) {
-            System.out.println("A fost aleasa specialitatea " + specialitate);
-            return;
-          } else {
-             System.out.println("Optiune invalida!");
+            int curentYear = LocalDate.now().getYear();
+            age = curentYear - yearOfBirth;
 
-         }
+            if (age <= 0 || age > 120)
+                System.out.println("Reintroduceti anul nasterii: ");
+            else {
+                System.out.println("Student: " + name + " " + "Varsta: " + age + " ani.");
+                break;
+            }
 
 
-         int test1;
-         while(true) {
-             System.out.print("Introduceti nota test1: ");
-                test1 = scan.nextInt();
+        }
 
-             if (test1 < 1 || test1 > 10)
-                 System.out.println("Nota invalida! Reintroduceti: ");
-             else
-                 break;
+        //Specialitate
+        String specialitate;
+        while (true) {
+            System.out.print("Introduceti specialitatea(Algebra/ Geometrie/ Calcul_Integral): ");
+            scan.nextLine();
+            specialitate = scan.nextLine();
 
-         }
+            if (specialitate.equalsIgnoreCase("algebra") || specialitate.equalsIgnoreCase("geometrie") || specialitate.equalsIgnoreCase("calcul_integral")) {
+                System.out.println("A fost aleasa specialitatea " + specialitate);
+                break;
+            } else {
+                System.out.println("Optiune invalida!");
 
-         int test2;
-        while(true) {
+            }
+        }
+
+
+        int test1;
+        while (true) {
+            System.out.print("Introduceti nota test1: ");
+            test1 = scan.nextInt();
+
+            if (test1 < 1 || test1 > 10)
+                System.out.println("Nota invalida! Reintroduceti: ");
+            else
+                break;
+
+        }
+
+        int test2;
+        while (true) {
             System.out.print("Introduceti nota test2: ");
-             test2 = scan.nextInt();
+            test2 = scan.nextInt();
 
             if (test2 < 1 || test2 > 10)
                 System.out.println("Nota invalida! Reintroduceti: ");
@@ -108,9 +122,9 @@ public class Student {
 
 
         int examen;
-        while(true) {
+        while (true) {
             System.out.print("Introduceti nota examen: ");
-             examen = scan.nextInt();
+            examen = scan.nextInt();
 
             if (examen < 1 || examen > 10)
                 System.out.println("Nota invalida! Reintroduceti: ");
@@ -119,12 +133,31 @@ public class Student {
 
         }
 
-        int notaFinala = (test1 + test2 + examen)/3;
+        int notaFinala = (test1 + test2 + examen) / 3;
 
-        if(notaFinala > 9)
+        if (notaFinala > 9)
             System.out.println("Esti promovat!");
         else
             System.out.println("Nu ati trecut cu brio, incercati in semestrul urmator");
+
+
+        String studentInfo = "Student: " + name + ", Varsta: " + age + ", Specialitate: " +
+                specialitate + ", Media: " + notaFinala;
+
+        studenti.add(studentInfo);
+        System.out.print("Doriti sa mai introduceti un student? ");
+        scan.nextLine();
+        String raspuns = scan.nextLine();
+
+        if (raspuns.equalsIgnoreCase("nu")) {
+            break;
+        }
+    }
+
+        System.out.println("Lista studentilor");
+        for (String s : studenti) {
+            System.out.println(s);
+        }
 
         System.out.println("Iesire din program...");
 
